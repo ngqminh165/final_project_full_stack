a55fddac6e11fa541574c407f48dec4cc259f9dc
Cody-Green-School <81730266+Cody-Green-School@users.noreply.github.com>
2021-05-04 14:14:21 -0700

    Trying to get Weather API working

--- js/weather.js
+++ js/weather.js
@@ -1,26 +1,46 @@
 //Cody Code Begin
+/*
 const weatherApiKey = "8d3792116b619ca66e2569b0ec1f39c0"
+let wzip = document.getElementById("inputName").value
 
 //Pull data from API
-function getWeather(zip) {
+function getWeather() {
+  alert("Got to getWeather")
   let wUrl =
-    "https://api.openweathermap.org/data/2.5/forecast/daily?zip=${" +
-    zip +
-    "}&appid=${" +
+    "https://api.openweathermap.org/data/2.5/forecast/daily?zip=" +
+    wzip +
+    "&appid=" +
     weatherApiKey +
     "}"
-  fetch(wurl)
+  fetch(wUrl)
     .then((response) => response.json())
     .then(function (data) {
       displayWeather(data)
     })
 }
-
+*/
 //Display Weather to webpage
 function displayWeather(d) {
-  let temp = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32)
+  alert("Got to displayWeather")
+  let temp = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32) //Convert Kelvin to Fahrenheit
+  console.log(temp)
 }
 
+let API_KEY = "8d3792116b619ca66e2569b0ec1f39c0"
+
+function getWeather(z) {
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    data: {
+      zip: z,
+      units: "imperial",
+      APPID: API_KEY,
+    },
+    success: (data) => {
+      console.log(data["main"]["temp"] + " F")
+    },
+  })
+}
 //function convertTime (API Given in UTC, need to convert to standard date to compare)
 
 //Cody Code End
