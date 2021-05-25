d27c51632deb2c090bd6b51e7ab0ae3809ecc507
ngqminh165 <minh25@pdx.edu>
2021-05-25 11:12:22 -0700

    doc

---
+++ final-project/party/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f475d96fd2dc73fa4b849c624415880'),
+    },
+  },
+});
