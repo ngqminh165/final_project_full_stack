c0f2b9ed5513872224f03f8358b977e356b22488
ngqminh165 <minh25@pdx.edu>
2021-05-25 18:07:38 -0700

    sign up

--- final-project/src/components/Login/SignUp.js
+++ final-project/src/components/Login/SignUp.js
@@ -70,7 +70,7 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign Up
           </Typography>
           <form className={classes.form} noValidate>
 
